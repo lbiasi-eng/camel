f8026d9459105774d567c799477e1d146a352b8e
lbiasi <luigi.biasi91@gmail.com>
2021-04-13 14:00:51 +0200

    use kafka avro deserializer

--- src/main/java/com/example/demo/route/KafkaRoute.java
+++ src/main/java/com/example/demo/route/KafkaRoute.java
@@ -1,6 +1,7 @@
 package com.example.demo.route;
-
+import com.example.demo.beans.Account;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,7 @@ import com.example.demo.processor.StoreProcessor;
 public class KafkaRoute extends RouteBuilder {
 
 	public static final String KAFKA_ROUTE_NAME = "kafka-router";
-	final String kafkaSource = "kafka:{{producer.topic}}?brokers={{kafka.bootstrap.url}}&keyDeserializer=org.apache.kafka.common.serialization.StringDeserializer&valueDeserializer=org.apache.kafka.common.serialization.StringDeserializer";
+	final String kafkaSource = "kafka:{{producer.topic}}?brokers={{kafka.bootstrap.url}}&keyDeserializer=org.apache.kafka.common.serialization.ByteArrayDeserializer&valueDeserializer=org.apache.kafka.common.serialization.ByteArrayDeserializer";
 
 	@Autowired
 	StoreProcessor store;
@@ -22,12 +23,15 @@ public class KafkaRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
+		String loggerStr = String.format("Received message:  ${in.body} with key: ${in.header.%s} " +
+				"and partition: ${in.header.%s} and offset: ${in.header.%s}",
+				KafkaConstants.KEY, KafkaConstants.PARTITION, KafkaConstants.OFFSET);
+
 		from(kafkaSource)
 				.routeId(KAFKA_ROUTE_NAME)
-				.process(store)
-				.process(read)
+				.log(loggerStr)
+				.unmarshal().avro(Account.class.getName())
 				.to("stream:out");
-				//.to("stream:out");
 	}
 
 }
